d2c2762e82e2063e82e5f8c71ffd8927a19feeb9
DorukAytekin07 <dorukaytekinexxon@gmail.com>
2024-05-02 09:02:52 -0700

    create button and import MoneyText component

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,19 @@
+import { useState } from "react";
+import MoneyText from "./Components/MoneyText";
+function App() {
+  const [currentMoney, setCurrentMoney] = useState(0);
 
+  return (
+    <>
+      <div className="center">
+        <button className="money-button" onClick={() => {
+          setCurrentMoney(currentMoney + 1)
+          console.log(currentMoney)
+
+        }}>Click</button>
+      </div >
+      <MoneyText currentMoney={currentMoney} />
+    </>
+  );
+}
+export default App;
