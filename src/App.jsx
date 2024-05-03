19455646fb0eed6b7768501319474be82f21ff91
DorukAytekin07 <dorukaytekinexxon@gmail.com>
2024-05-03 03:01:18 -0700

    add sidebar component to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import MoneyText from "./Components/MoneyText";
 import Button from "./Components/Button"
+import Sidebar from "./Components/SideBar";
 function App() {
   const [currentMoney, setCurrentMoney] = useState(0);
 
   return (
     <>
+      <div className="top-part">
+        <BrowserRouter>
+          <Sidebar>
+            <Routes>
+              <Route path="/store" element={<App />} />
+            </Routes>
+          </Sidebar>
+        </BrowserRouter>
+
+        <MoneyText currentMoney={currentMoney} />
+      </div>
       <Button callback={setCurrentMoney} currentMoney={currentMoney} />
-      <MoneyText currentMoney={currentMoney} />
     </>
   );
 }
